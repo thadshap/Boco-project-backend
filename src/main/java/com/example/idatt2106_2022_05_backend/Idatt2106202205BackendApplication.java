86134759995926d2f2cf5887606acbe9d6a8045d
Hasan Rehman Omarzae <hasanro@stud.ntnu.no>
2022-04-19 11:23:31 +0200

    Initial commit

---
+++ src/main/java/com/example/idatt2106_2022_05_backend/Idatt2106202205BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.idatt2106_2022_05_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Idatt2106202205BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Idatt2106202205BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/idatt2106_2022_05_backend/Idatt2106202205BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.idatt2106_2022_05_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Idatt2106202205BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
