f7f67b9b3892d1b5d1721a48d9011bbb4792712b
Karol <karoline.wahl@hotmail.no>
2022-04-19 15:28:28 +0200

    Implemented picture model

---
+++ src/main/java/com/example/idatt2106_2022_05_backend/model/Picture.java
@@ -0,0 +1,30 @@
+package com.example.idatt2106_2022_05_backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import nonapi.io.github.classgraph.json.Id;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "courses")
+public class Picture {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "picture_id", nullable = false)
+    private Long pictureId;
+
+    @Column(name = "content", nullable = false)
+    private String title;
+
+    @JoinColumn(name = "ad_id")
+    private Ad ad;
+
+}
