c47df9edc3967ed36e5daa9c197f60645149c748
Hasan Rehman Omarzae <hasanro@stud.ntnu.no>
2022-04-20 10:19:19 +0200

    JWT security added to project ass well as gitlab-ci.yml class.

--- src/main/java/com/example/idatt2106_2022_05_backend/repository/UserRepository.java
+++ src/main/java/com/example/idatt2106_2022_05_backend/repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.example.idatt2106_2022_05_backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
 }
---
+++ src/main/java/com/example/idatt2106_2022_05_backend/security/JWTConfig.java
@@ -0,0 +1,60 @@
+package com.example.idatt2106_2022_05_backend.security;
+
+import com.example.idatt2106_2022_05_backend.service.user.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * TODO skriv utfyllende her
+ */
+@Component
+public class JWTConfig extends OncePerRequestFilter {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        String auth = httpServletRequest.getHeader("Authorization");
+        String token = null;
+        String email = null;
+
+        if(null != auth && auth.startsWith("Bearer ")) {
+            token = auth.substring(7);
+            email = jwtUtil.getEmailFromToken(token);
+        }
+
+        if(null != email && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails
+                    = userDetailsService.loadUserByUsername(email);
+
+            if(jwtUtil.validateToken(token,userDetails)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken
+                        = new UsernamePasswordAuthenticationToken(userDetails,
+                        null, userDetails.getAuthorities());
+
+                usernamePasswordAuthenticationToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(httpServletRequest)
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+
+        }
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
