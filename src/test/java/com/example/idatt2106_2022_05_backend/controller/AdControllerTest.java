3be576c65f5c8f62bda966d2544b4df01b6dfb9f
Karol <karoline.wahl@hotmail.no>
2022-04-21 14:40:08 +0200

    Get/Post methods in ad controller test-class created.

--- src/test/java/com/example/idatt2106_2022_05_backend/controller/AdControllerTest.java
+++ src/test/java/com/example/idatt2106_2022_05_backend/controller/AdControllerTest.java
@@ -3,6 +3,8 @@ package com.example.idatt2106_2022_05_backend.controller;
 import com.example.idatt2106_2022_05_backend.dto.AdDto;
 import com.example.idatt2106_2022_05_backend.enums.AdType;
 import com.example.idatt2106_2022_05_backend.model.Ad;
+import com.example.idatt2106_2022_05_backend.model.Review;
+import com.example.idatt2106_2022_05_backend.model.User;
 import com.example.idatt2106_2022_05_backend.service.ad.AdService;
 import com.example.idatt2106_2022_05_backend.util.Response;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,6 +18,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -32,6 +37,9 @@ class AdControllerTest {
 
     // Creating an uninitialized Ad-object
     private Ad ad;
+    private User user;
+
+    private Set<Ad> ads;
 
 
     @BeforeEach
@@ -50,6 +58,24 @@ class AdControllerTest {
                 streetAddress("Project Road 4").
                 postalCode(7234).
                 build();
+
+        // Building a user
+        user = User.builder().
+                id(2L).
+                firstName("firstName").
+                lastName("lastName").
+                email("user.name@hotmail.com").
+                password("pass1word").
+                build();
+
+        // Set the foreign key for the ad
+        ad.setUser(user);
+
+        // Add the new ad to the list of ads
+        ads.add(ad);
+
+        // Add the list of ads to the user
+        user.setAds(ads);
     }
 
     @Test
@@ -68,27 +94,64 @@ class AdControllerTest {
     }
 
     @Test
-    void getAllAds() {
+    void getAllAds() throws Exception {
+        Mockito.when(adService.getAllAds()).
+                thenReturn(new Response(ads,HttpStatus.OK));
+
+        // Performing the get operation
+        mockMvc.perform(get("/ads").
+                        contentType(MediaType.APPLICATION_JSON)).
+                andExpect(status().isOk());
     }
 
     @Test
-    void getAllAvailableAds() {
+    void getAllAvailableAds() throws Exception {
+        Mockito.when(adService.getAllAvailableAds()).
+                thenReturn(new Response(ads,HttpStatus.OK));
+
+        // Performing the get operation
+        mockMvc.perform(get("/ads/available").
+                        contentType(MediaType.APPLICATION_JSON)).
+                andExpect(status().isOk()); //todo test that size = 1 ?
     }
 
     @Test
-    void getAvailableAdsByUserId() {
+    void getAvailableAdsByUserId() throws Exception {
+        Mockito.when(adService.getAllAvailableAdsByUser(2L)).
+                thenReturn(new Response(ads,HttpStatus.OK));
+
+        // Performing the get operation
+        mockMvc.perform(get("/ads/available/2").
+                        contentType(MediaType.APPLICATION_JSON)).
+                andExpect(status().isOk()); //todo test that size = 1
     }
 
     @Test
-    void getAdByPostalCode() {
+    void getAdByPostalCode() throws Exception {
+        Mockito.when(adService.getAllAdsByPostalCode(7234)).
+                thenReturn(new Response(ads,HttpStatus.OK));
+
+        // Performing the get operation
+        mockMvc.perform(post("/ads").
+                        contentType(MediaType.APPLICATION_JSON).
+                        content("{\n" +
+                                "\t\"postalCode\" : 7234\n" +
+                                "}")).
+                andExpect(status().isOk());
     }
 
     @Test
-    void getAllAdsByRentalType() {
-    }
+    void getAllAdsByRentalType() throws Exception {
+        Mockito.when(adService.getAllAdsByRentalType(true)).
+                thenReturn(new Response(ads,HttpStatus.OK));
 
-    @Test
-    void getAdByUserId() {
+        // Performing the get operation
+        mockMvc.perform(post("/ads").
+                        contentType(MediaType.APPLICATION_JSON).
+                        content("{\n" +
+                                "\t\"rentalType\" : 7234\n" +
+                                "}")).
+                andExpect(status().isOk());
     }
 
     @Test
@@ -127,11 +190,30 @@ class AdControllerTest {
     }
 
     @Test
-    void getReviewsByUserId() {
+    void getReviewsByUserId() throws Exception {
+        Set<Review> reviews = new HashSet<>();
+
+        Review review = Review.builder().
+                id(5L).
+                description("Great shoes!").
+                rating(5).
+                user(user).
+                build();
+
+        reviews.add(review);
+
+        Mockito.when(adService.getReviewsByUserId(2L)).
+                thenReturn(new Response(reviews,HttpStatus.OK));
+
+        // Performing the get operation
+        mockMvc.perform(get("users/ads/reviews/2").
+                        contentType(MediaType.APPLICATION_JSON)).
+                andExpect(status().isOk());
     }
 
     @Test
     void updateTitle() {
+
     }
 
     @Test
