22cf049e1af1e8c204a64fd4cf9f30b32cf997ac
Maiken Louise  Brechan <maikebre@stud.ntnu.com>
2022-04-25 13:47:33 +0200

    fixed search method

--- src/main/java/com/example/idatt2106_2022_05_backend/controller/AdController.java
+++ src/main/java/com/example/idatt2106_2022_05_backend/controller/AdController.java
@@ -11,8 +11,6 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import javax.xml.ws.RequestWrapper;
 import java.io.IOException;
 
 @Slf4j
