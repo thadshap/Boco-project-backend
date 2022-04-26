de49c6c75f42e391ca4df27909fd07ff1e3f8967
Maiken Louise  Brechan <maikebre@stud.ntnu.com>
2022-04-26 11:31:00 +0200

    commit berfore merging with main!

--- src/main/java/com/example/idatt2106_2022_05_backend/controller/ChatController.java
+++ src/main/java/com/example/idatt2106_2022_05_backend/controller/ChatController.java
@@ -5,6 +5,7 @@ import com.example.idatt2106_2022_05_backend.service.chat.ChatService;
 import com.example.idatt2106_2022_05_backend.util.Response;
 import com.example.idatt2106_2022_05_backend.util.WebSocket;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
