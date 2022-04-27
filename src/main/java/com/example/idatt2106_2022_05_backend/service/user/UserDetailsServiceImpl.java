c5744b38016b7351639a1c110c3a5529580214f5
Hasan Rehman Omarzae <hasanro@stud.ntnu.no>
2022-04-27 11:43:21 +0200

    Keystore complete.

--- src/main/java/com/example/idatt2106_2022_05_backend/service/user/UserDetailsServiceImpl.java
+++ src/main/java/com/example/idatt2106_2022_05_backend/service/user/UserDetailsServiceImpl.java
@@ -2,6 +2,8 @@ package com.example.idatt2106_2022_05_backend.service.user;
 
 import com.example.idatt2106_2022_05_backend.model.User;
 import com.example.idatt2106_2022_05_backend.repository.UserRepository;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +18,9 @@ import java.util.ArrayList;
  */
 @Slf4j
 @NoArgsConstructor
+@Builder
 @Service
+@Data
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
