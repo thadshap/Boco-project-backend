e0a530a8e7ce280f1ec43cfd87158ed7119c6d49
johannes <jhmorkem@stud.ntnu.no>
2022-04-29 12:49:01 +0200

    added groupDtos

---
+++ src/main/java/com/example/idatt2106_2022_05_backend/dto/GroupDto.java
@@ -0,0 +1,13 @@
+package com.example.idatt2106_2022_05_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GroupDto {
+    private long groupId;
+    private String groupName;
+}
