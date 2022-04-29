c2d2c25c7c3c34c24b9c3e0edab87c9a87d18bcf
Karol <karoline.wahl@hotmail.no>
2022-04-29 08:29:56 +0200

    All ad integration tests work. Starting work on user integration tests.

--- src/main/java/com/example/idatt2106_2022_05_backend/dto/UpdatePictureDto.java
+++ src/main/java/com/example/idatt2106_2022_05_backend/dto/UpdatePictureDto.java
@@ -3,9 +3,15 @@ package com.example.idatt2106_2022_05_backend.dto;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Data
 @ApiModel(description = "data transfer object to delete or upload a picture")
 public class UpdatePictureDto {
