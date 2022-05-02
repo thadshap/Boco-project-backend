a77e4b5eae8a14289e49d4bcbdba7936c6f3d7a4
Karol <karoline.wahl@hotmail.no>
2022-05-02 12:11:09 +0200

    Testing coverage improved. Starting to test category methods now.

--- src/test/java/com/example/idatt2106_2022_05_backend/integration/CategoryIntegrationTest.java
+++ src/test/java/com/example/idatt2106_2022_05_backend/integration/CategoryIntegrationTest.java
@@ -1,4 +1,76 @@
 package com.example.idatt2106_2022_05_backend.integration;
 
+
+import com.example.idatt2106_2022_05_backend.repository.*;
+import com.example.idatt2106_2022_05_backend.service.ad.AdService;
+import com.example.idatt2106_2022_05_backend.service.user.UserService;
+import com.example.idatt2106_2022_05_backend.util.PictureUtility;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class CategoryIntegrationTest {
+
+    @Autowired
+    AdService adService;
+
+    @Autowired
+    AdRepository adRepository;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    RentalRepository rentalRepository;
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    PictureUtility pictureService;
+
+    @Autowired
+    CalendarDateRepository calendarDateRepository;
+
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    @Test
+    public void categoryCreated_WhenCorrectParams() {
+
+    }
+
+    @Test
+    public void categoryNotCreated_WhenWrongParams() {
+
+    }
+
+    @Test
+    public void getAllParentCategories() {
+
+    }
+
+    @Test
+    public void getAllSubCategories() {
+        // Get lvl1 subs
+        // Get lvl2 subs
+    }
+
+    @Test
+    public void getAllAdsForCategory() {
+
+    }
+
+    @Test
+    public void getAllAdsForCategoryAndSubCategories() {
+
+    }
 }
