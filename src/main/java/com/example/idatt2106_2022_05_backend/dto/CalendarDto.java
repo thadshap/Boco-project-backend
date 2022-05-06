75f432ccb98537296ce2813b261912973b6bada5
Maiken Louise  Brechan <maikebre@stud.ntnu.com>
2022-05-06 20:44:35 +0200

    Changes some pictures on dataloader

--- src/main/java/com/example/idatt2106_2022_05_backend/dto/CalendarDto.java
+++ src/main/java/com/example/idatt2106_2022_05_backend/dto/CalendarDto.java
@@ -1,13 +1,17 @@
 package com.example.idatt2106_2022_05_backend.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CalendarDto {
 
     // AdId
